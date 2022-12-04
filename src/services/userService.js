8e90c1785957c22c25206086e154e3d881b2c237
Graziely Jacob <jacobgrazy@gmail.com>
2022-12-04 17:09:25 -0300

    Requisito com lint corrigido

--- src/services/userService.js
+++ src/services/userService.js
@@ -3,7 +3,7 @@ const { createToken } = require('../auth/jwtFunctions');
 const { User } = require('../models');
 
 const createUser = async ({ displayName, email, password, image }) => {
-  const user = await User.create({ email, password });
+  const user = await User.create({ displayName, email, password, image });
   const { password: _password, ...userWithoutPassword } = user.dataValues;
   const token = createToken(userWithoutPassword);
   return { token };
