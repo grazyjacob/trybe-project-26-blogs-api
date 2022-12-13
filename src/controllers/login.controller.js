7694a0ec193410b9393d12d3839e710db8f30bf7
Graziely Jacob <jacobgrazy@gmail.com>
2022-12-13 13:25:54 -0300

    teste avaliador 2

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -1,7 +1,4 @@
 const UserService = require('../services/loginService');
-// const { User } = require('../models');
-const { createToken } = require('../auth/jwtFunctions');
-
 require('dotenv/config');
 
 const verify = async (req, res) => {
@@ -17,15 +14,13 @@ return null;
 const postLogin = async (req, res) => {
     const { email, password } = req.body;
     const verifyFields = await verify(req, res);
-    const byEmail = await UserService.getByEmail(email);
     if (verifyFields) return verifyFields;
     const byPassword = await UserService.getByPassword(password);
+    const byEmail = await UserService.getByEmail(email);
     if (byEmail === null || byPassword === null) {
       return res.status(400).json({ message: 'Invalid fields' });
     }
-    console.log('POST LOGIN CONTROLLER', byEmail.displayName);
-    const user = [byEmail.displayName, byEmail.email, byEmail.image]; 
-    const token = createToken(user);
+      const { token } = await UserService.createUser(email, password);
       return res
       .status(200)
       .json({ token });
